1655b4847fbb8c6bebdfcc6ebafd97362e9c0315
Fang Long <fang.long@alliancesoftware.com.au>
2020-01-10 10:17:43 +1100

    refactor: refactor: CR
    affects: @prestojs/ui-antd

--- js-packages/@prestojs/ui-antd/src/__tests__/getWidgetForField.test.tsx
+++ js-packages/@prestojs/ui-antd/src/__tests__/getWidgetForField.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import getWidgetForField from '../getWidgetForField';
 import { NumberField } from '@prestojs/viewmodel';
 import { render, fireEvent, screen } from '@testing-library/react';
-
+import '@testing-library/jest-dom/extend-expect';
 
 test('getWidgetForField should return widget for field', async() => {
     const fieldArgs = {label: 'input number with a spin button'};
@@ -15,9 +15,6 @@ test('getWidgetForField should return widget for field', async() => {
     );
 
     const lazyElement = await screen.findByRole('spinbutton');
-    // "Property 'toBeInTheDocument' does not exist on type 'JestMatchersShape<Matchers<void, any>, Matchers<Promise<void>, any>>'". outdated def?
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
     expect(lazyElement).toBeInTheDocument();
 });
 
@@ -34,8 +31,5 @@ test('getWidgetForField should return widget for descendant classes of same type
     );
 
     const lazyElement = await screen.findByRole('spinbutton');
-    // "Property 'toBeInTheDocument' does not exist on type 'JestMatchersShape<Matchers<void, any>, Matchers<Promise<void>, any>>'". outdated def?
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
     expect(lazyElement).toBeInTheDocument();
 });
