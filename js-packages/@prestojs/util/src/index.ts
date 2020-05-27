f3a75fb5b7709bed90642419deecf376bbd49b43
David Coates <david.coates@alliancesoftware.com.au>
2020-05-27 16:03:41 +1000

    sys: export updates, package updates

--- js-packages/@prestojs/util/src/index.ts
+++ js-packages/@prestojs/util/src/index.ts
@@ -1,2 +1,9 @@
 export { default as useChangeObserver } from './useChangeObserver';
 export { default as useListChangeObserver } from './useListChangeObserver';
+export { getId, hashId, isSameById } from './identifiable';
+export { default as useAsync } from './useAsync';
+export { isPromise } from './misc';
+
+import type { Id, Identifiable } from './identifiable';
+
+export type { Identifiable, Id };
