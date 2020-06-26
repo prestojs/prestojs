1727d5f9225c016c3e542b3d318cc08044c55ddd
David Coates <david.coates@alliancesoftware.com.au>
2020-06-26 14:54:14 +1000

    feature: add async choices to testbed

--- django-testbed/frontend/src/index.js
+++ django-testbed/frontend/src/index.js
@@ -1,6 +1,7 @@
 import { Endpoint } from '@prestojs/rest';
 import React from 'react';
 import { render } from 'react-dom';
+import 'stop-runaway-react-effects/hijack';
 
 import App from './App';
 
