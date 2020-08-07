c04dadcd2ce1fdab3d96a0a10ca640be73f765e6
David Coates <david.coates@alliancesoftware.com.au>
2020-08-07 14:05:39 +1000

    sys: bump presto_drf

--- python-packages/presto_drf/setup.py
+++ python-packages/presto_drf/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="presto_drf",
-    version="0.0.1",
+    version="0.0.2",
     author="Alliance Software",
     author_email="support@alliancesoftware.com.au",
     packages=["presto_drf"],  # this must be the same as the name above
