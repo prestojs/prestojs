5d58adf36fff04af402a9ca58bfe68c83ed90f50
David Coates <david.coates@alliancesoftware.com.au>
2020-08-10 13:54:06 +1000

    fix: bad import

--- js-packages/@prestojs/ui-antd/src/widgets/NullableBooleanWidget.tsx
+++ js-packages/@prestojs/ui-antd/src/widgets/NullableBooleanWidget.tsx
@@ -1,4 +1,4 @@
-import { WidgetProps } from '@prestojs/ui';
+import type { WidgetProps } from '@prestojs/ui';
 import { Select } from 'antd';
 import React from 'react';
 import SelectChoiceWidget, { SelectChoiceProps } from './SelectChoiceWidget';
