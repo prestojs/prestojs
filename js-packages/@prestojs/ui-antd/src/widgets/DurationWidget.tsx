5c57037b974174647b154927cff52eeb739c7dce
Fang Long <fang.long@alliancesoftware.com.au>
2021-04-12 08:57:58 +1000

    fix: duration widget to use freeform input

--- js-packages/@prestojs/ui-antd/src/widgets/DurationWidget.tsx
+++ js-packages/@prestojs/ui-antd/src/widgets/DurationWidget.tsx
@@ -1,20 +1,23 @@
 import { WidgetProps } from '@prestojs/ui';
-import { TimePicker } from 'antd';
-import { TimePickerProps } from 'antd/lib/time-picker';
+import { Input } from 'antd';
 import React from 'react';
 
 /**
  * @expand-properties
  * @hide-properties choices asyncChoices
  */
-type DurationWidgetProps = WidgetProps<string, HTMLInputElement> & { input: TimePickerProps };
+type DurationWidgetProps = WidgetProps<string, HTMLInputElement>;
 
 /**
- * See [TimePicker](https://ant.design/components/time-picker/) for props available
+ * See [Input](https://ant.design/components/input/) for props available
+ *
+ * @extract-docs
+ * @menu-group Widgets
+ * @forward-ref
  */
 function DurationWidget(props: DurationWidgetProps, ref: React.RefObject<any>): React.ReactElement {
     const { input, ...rest } = props;
-    return <TimePicker ref={ref} format="HH:mm" {...input} {...rest} />;
+    return <Input ref={ref} {...input} {...rest} />;
 }
 
 export default React.forwardRef(DurationWidget);
