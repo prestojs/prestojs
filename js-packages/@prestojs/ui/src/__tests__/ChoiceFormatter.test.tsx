2668088c510b59c363e099f1df9ca9e458935484
David Coates <david.coates@alliancesoftware.com.au>
2021-04-30 13:43:18 +1000

    feature: ChoiceFormatter types & docs

--- js-packages/@prestojs/ui/src/__tests__/ChoiceFormatter.test.tsx
+++ js-packages/@prestojs/ui/src/__tests__/ChoiceFormatter.test.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { IntegerField } from '@prestojs/viewmodel';
 import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 
 import FieldFormatter from '../FieldFormatter';
-import UiProvider from '../UiProvider';
+import ChoiceFormatter from '../formatters/ChoiceFormatter';
 import getFormatterForField from '../getFormatterForField';
+import UiProvider from '../UiProvider';
 
 test('when supplied a field with choices choice formatter is used and correctly rendered', async () => {
     const choices = new Map([
@@ -30,4 +31,64 @@ test('when supplied a field with choices choice formatter is used and correctly 
 
     const lazyElement = await screen.findByText('Two');
     expect(lazyElement).toBeInTheDocument();
+});
+
+test('ChoiceFormatter supports blankLabel', () => {
+    const choices = new Map([
+        [1, 'One'],
+        [2, 'Two'],
+        [3, 'Three'],
+        [4, 'Four'],
+    ]);
+
+    const { getByTestId, rerender } = render(
+        <div data-testid="value">
+            <ChoiceFormatter value={null} choices={choices} />
+        </div>
+    );
+    expect(getByTestId('value')).toHaveTextContent('');
+    rerender(
+        <div data-testid="value">
+            <ChoiceFormatter value={null} choices={choices} blankLabel="Not Found" />
+        </div>
+    );
+    expect(getByTestId('value')).toHaveTextContent('Not Found');
+});
+
+test('ChoiceFormatter supports invalidChoiceLabel', () => {
+    const choices = new Map([
+        [1, 'One'],
+        [2, 'Two'],
+        [3, 'Three'],
+        [4, 'Four'],
+    ]);
+    const spy = jest.spyOn(console, 'warn').mockImplementation();
+
+    const { getByTestId, rerender } = render(
+        <div data-testid="value">
+            <ChoiceFormatter value={5} choices={choices} />
+        </div>
+    );
+    expect(getByTestId('value')).toHaveTextContent('');
+    rerender(
+        <div data-testid="value">
+            <ChoiceFormatter value={5} choices={choices} invalidChoiceLabel="Invalid Choice" />
+        </div>
+    );
+    expect(spy).toHaveBeenCalledTimes(2);
+    spy.mockReset();
+    expect(getByTestId('value')).toHaveTextContent('Invalid Choice');
+    rerender(
+        <div data-testid="value">
+            <ChoiceFormatter
+                value={5}
+                choices={choices}
+                invalidChoiceLabel="Invalid Choice"
+                warnOnInvalidChoice={false}
+            />
+        </div>
+    );
+    expect(spy).not.toHaveBeenCalled();
+
+    spy.mockRestore();
 });
--- js-packages/@prestojs/ui/src/formatters/ChoiceFormatter.tsx
+++ js-packages/@prestojs/ui/src/formatters/ChoiceFormatter.tsx
@@ -1,20 +1,63 @@
-export default function ChoiceFormatter<T>({
-    value,
-    choices,
-}: {
+import React, { ReactElement, ReactNode } from 'react';
+
+/**
+ * @expand-properties
+ */
+type ChoiceFormatterProps<T> = {
+    /**
+     * The value to format
+     */
     value: T;
-    choices: Array<[T, string]>;
-}): string | T {
+    /**
+     * The choices to render. This can be a `Map` of value to label or an array of 2-element arrays `[value, label]`.
+     */
+    choices: [any, string][] | Map<any, string>;
+    /**
+     * What to render when `value` is `null` or `undefined`
+     */
+    blankLabel?: ReactNode;
+    /**
+     * What to render if `value` is not found in `choices`. By default this is considered a programming error and a warning
+     * is logged. If your use case supports invalid values pass `warnOnInvalidChoice={false}` to suppress the warning.
+     */
+    invalidChoiceLabel?: ReactNode;
+    /**
+     * If `value` is not found in `choices` then a warning will be logged by default. To supress this warning pass
+     * `warnOnInvalidChoice={false}`.
+     *
+     * Defaults to `true`.
+     */
+    warnOnInvalidChoice?: boolean;
+};
+
+/**
+ * Render the label for choice based on the provided value
+ *
+ * This is the [default formatter](doc:getFormatterForField) used when a field has choices defined.
+ *
+ * @extract-docs
+ * @menu-group Formatters
+ */
+export default function ChoiceFormatter<T>(props: ChoiceFormatterProps<T>): ReactElement {
+    const {
+        value,
+        choices,
+        blankLabel = null,
+        invalidChoiceLabel = null,
+        warnOnInvalidChoice = true,
+    } = props;
     if (value == null) {
-        return value;
+        return <>{blankLabel}</>;
     }
     const choiceValue = Array.from(choices).find(choice => choice[0] === value);
     if (choiceValue === null || choiceValue === undefined) {
-        console.warn(
-            `Expected to find value choice label with value ${value} but no such choice option exists among `,
-            choices
-        );
-        return value;
+        if (warnOnInvalidChoice) {
+            console.warn(
+                `Expected to find value choice label with value ${value} but no such choice option exists among `,
+                choices
+            );
+        }
+        return <>{invalidChoiceLabel}</>;
     }
-    return choiceValue[1];
+    return <>{choiceValue[1]}</>;
 }
