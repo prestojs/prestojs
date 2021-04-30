a3f8c96ee11dfffa7cece15759cdb404efa686aa
David Coates <david.coates@alliancesoftware.com.au>
2021-04-30 13:13:14 +1000

    feature: CharFormatter types & docs

--- js-packages/@prestojs/ui/src/formatters/CharFormatter.tsx
+++ js-packages/@prestojs/ui/src/formatters/CharFormatter.tsx
@@ -1,3 +1,35 @@
-export default function CharFormatter({ value }: { value?: string }): string {
-    return value || '';
+import React, { ReactElement, ReactNode } from 'react';
+
+/**
+ * @expand-properties
+ */
+type CharFormatterProps = {
+    /**
+     * What to render when `value` is `null`, `undefined` or an empty string
+     *
+     * Defaults to `null`
+     */
+    blankLabel?: ReactNode;
+    /**
+     * The value to render
+     */
+    value?: string | null;
+};
+
+/**
+ * Format a string for display. Returns value directly as is.
+ *
+ * This is the [default formatter](doc:getFormatterForField) used for [CharField](doc:CharField), [EmailField](doc:EmailField),
+ * [TextField](doc:TextField), [SlugField](doc:SlugField), [IPAddressField](doc:IPAddressField), [UUIDField](doc:UUIDField) and
+ * [JSONField](doc:JsonField)
+ *
+ * @extract-docs
+ * @menu-group Formatters
+ */
+export default function CharFormatter(props: CharFormatterProps): ReactElement {
+    const { value, blankLabel = null } = props;
+    if (value == null || value === '') {
+        return <>{blankLabel}</>;
+    }
+    return <>{value || ''}</>;
 }
