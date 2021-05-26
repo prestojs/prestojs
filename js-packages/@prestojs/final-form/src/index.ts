b7cfb9ae57d055f29844d026fd8b985585f0bc80
David Coates <david.coates@alliancesoftware.com.au>
2021-05-26 14:21:58 +1000

    fix: export FormProps

--- js-packages/@prestojs/final-form/src/index.ts
+++ js-packages/@prestojs/final-form/src/index.ts
@@ -1,3 +1,5 @@
 export { default as Form } from './Form';
 export { default as FormField } from './FormField';
 export { useForm } from 'react-final-form';
+
+export type { FormProps } from './Form';
