38699d2b48cd953b055a856a07ccefb1c8320029
David Coates <david.coates@alliancesoftware.com.au>
2021-10-21 12:15:04 +1100

    fix: add missing exports

--- js-packages/@prestojs/rest/src/index.ts
+++ js-packages/@prestojs/rest/src/index.ts
@@ -1,13 +1,14 @@
 export { default as Endpoint, ApiError, mergeRequestInit, SkipToResponse } from './Endpoint';
 export { default as viewModelCachingMiddleware } from './viewModelCachingMiddleware';
 export { default as dedupeInFlightRequestsMiddleware } from './dedupeInFlightRequestsMiddleware';
-export { default as paginationMiddleware } from './paginationMiddleware';
+export { default as paginationMiddleware, PaginationMiddleware } from './paginationMiddleware';
 export { default as requestDefaultsMiddleware } from './requestDefaultsMiddleware';
 export { default as detectBadPaginationMiddleware } from './detectBadPaginationMiddleware';
 export { default as batchMiddleware } from './batchMiddleware';
 
 import type {
     EndpointExecuteOptions,
+    EndpointRequestInit,
     ExecuteReturnVal,
     Middleware,
     MiddlewareContext,
@@ -20,6 +21,7 @@ import type {
 
 export type {
     EndpointExecuteOptions,
+    EndpointRequestInit,
     ExecuteReturnVal,
     Middleware,
     MiddlewareContext,
