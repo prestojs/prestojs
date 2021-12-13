358f3002f8dfc63e4e4ee9a2cb85ea8e935172a0
David Coates <david.coates@alliancesoftware.com.au>
2021-12-13 16:05:23 +1100

    fix: update testbed model def

--- django-testbed/frontend/src/models/generated/BaseUser.js
+++ django-testbed/frontend/src/models/generated/BaseUser.js
@@ -1,19 +1,22 @@
 import { CharField, EmailField, NumberField, viewModelFactory } from '@prestojs/viewmodel';
 
-export default class Base extends viewModelFactory({
-    id: new NumberField({
-        label: 'Id',
-    }),
-    firstName: new CharField({
-        label: 'First Name',
-    }),
-    lastName: new CharField({
-        label: 'Last Name',
-    }),
-    email: new EmailField({
-        label: 'Email',
-    }),
-}) {
+export default class Base extends viewModelFactory(
+    {
+        id: new NumberField({
+            label: 'Id',
+        }),
+        firstName: new CharField({
+            label: 'First Name',
+        }),
+        lastName: new CharField({
+            label: 'Last Name',
+        }),
+        email: new EmailField({
+            label: 'Email',
+        }),
+    },
+    { pkFieldName: 'id' }
+) {
     static label = 'User';
     static labelPlural = 'Users';
 }
