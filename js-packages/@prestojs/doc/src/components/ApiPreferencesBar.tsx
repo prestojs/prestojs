321258e7d3835798a6c1d3d959e294f7236ecec3
David Coates <david.coates@alliancesoftware.com.au>
2022-05-31 10:19:06 +1000

    fix: anchor link on API

--- js-packages/@prestojs/doc/src/components/ApiPreferencesBar.tsx
+++ js-packages/@prestojs/doc/src/components/ApiPreferencesBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import AnchorLink from '../components/AnchorLink';
 import { usePreferences } from './PreferencesProvider';
 
 export default function ApiPreferencesBar() {
     const { showInherited, setShowInherited } = usePreferences();
     return (
         <div className="my-2 my-4 flex justify-between border-b border-gray-400">
-            <h2 className="font-semibold text-lg">API</h2>
+            <AnchorLink component="h2" id="api" className="font-semibold text-lg">
+                API
+            </AnchorLink>
             <label className="cursor-pointer">
                 <input
                     type="checkbox"
