6824705cd439410dac70e7c5ea4402693ad92621
David Coates <david.coates@alliancesoftware.com.au>
2022-06-08 07:49:03 +1000

    doc: Decimal/Float/Integer field examples

---
+++ doc-site/pages/examples/viewmodel/fields/FloatField/form-usage.tsx
@@ -0,0 +1,48 @@
+/**
+ * Default widget in a Form
+ *
+ * This example shows the default widget that will be used in a [Form](doc:Form)
+ * when using [@prestojs/ui-antd](/docs/ui-antd). See [getWidgetForField](doc:getWidgetForField).
+ *
+ * The default widget is [FloatWidget](doc:FloatWidget).
+ *
+ * @wide
+ * @min-height 320
+ */
+import { Form } from '@prestojs/final-form';
+import { AntdUiProvider, FormItemWrapper, FormWrapper, getWidgetForField } from '@prestojs/ui-antd';
+import { FloatField, IntegerField, viewModelFactory } from '@prestojs/viewmodel';
+import { Button } from 'antd';
+import 'antd/dist/antd.min.css';
+import React from 'react';
+
+class ExampleModel extends viewModelFactory(
+    {
+        id: new IntegerField(),
+        total: new FloatField({ minValue: 0 }),
+    },
+    { pkFieldName: 'id' }
+) {}
+
+export default function FormUsage() {
+    return (
+        <React.Suspense fallback="Loading...">
+            <AntdUiProvider
+                getWidgetForField={getWidgetForField}
+                formItemComponent={FormItemWrapper}
+                formComponent={FormWrapper}
+            >
+                <div className="grid grid-cols-1 gap-4 w-full">
+                    <Form onSubmit={data => console.log(data)}>
+                        <Form.Item field={ExampleModel.fields.total} />
+                        <Form.Item wrapperCol={{ offset: 6 }}>
+                            <Button type="primary" htmlType="submit">
+                                Submit (check console)
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </AntdUiProvider>
+        </React.Suspense>
+    );
+}
