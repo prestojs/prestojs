da2d3ed5944dc83364f0065574190f65151dd66a
David Coates <david.coates@alliancesoftware.com.au>
2022-11-16 09:53:15 +1100

    fix: cleanup use of deprecated apis

--- js-packages/@prestojs/rest/src/__tests__/dedupeInFlightRequestsMiddleware.test.ts
+++ js-packages/@prestojs/rest/src/__tests__/dedupeInFlightRequestsMiddleware.test.ts
@@ -22,41 +22,37 @@ test('should de-dupe in flight requests', async () => {
             },
         })
     );
-    const action1 = new Endpoint(new UrlPattern('/whatever/'), {
+    const endpoint1 = new Endpoint(new UrlPattern('/whatever/'), {
         middleware: [dedupeInFlightRequestsMiddleware()],
     });
-    const p1 = action1.prepare().execute();
-    const p2 = action1.prepare().execute();
+    const p1 = endpoint1.execute();
+    const p2 = endpoint1.execute();
     expect((await p1).result).toBe('hello world 0');
     expect((await p2).result).toBe('hello world 0');
     // All aspects of the response should be equivalent
     expect(await p1).toEqual(await p2);
     expect(fetchMock).toHaveBeenCalledTimes(1);
-    expect((await action1.prepare().execute()).result).toBe('hello world 1');
+    expect((await endpoint1.execute()).result).toBe('hello world 1');
     expect(fetchMock).toHaveBeenCalledTimes(2);
     const init1 = { headers: new Headers({ 'x-test': '123' }) };
-    const p3 = action1.prepare().execute(init1);
-    const p4 = action1.prepare().execute(init1);
+    const p3 = endpoint1.execute(init1);
+    const p4 = endpoint1.execute(init1);
     expect((await p3).result).toBe('hello world 2');
     expect((await p4).result).toBe('hello world 2');
     expect(await p3).toEqual(await p4);
     expect(fetchMock).toHaveBeenCalledTimes(3);
-    expect((await action1.prepare().execute(init1)).result).toBe('hello world 3');
+    expect((await endpoint1.execute(init1)).result).toBe('hello world 3');
     expect(fetchMock).toHaveBeenCalledTimes(4);
     const init5 = { headers: new Headers({ 'x-test': 'abc' }) };
     const init6 = { headers: new Headers({ 'x-test': '123' }) };
-    const p5 = action1.prepare().execute(init5);
-    const p6 = action1.prepare().execute(init6);
+    const p5 = endpoint1.execute(init5);
+    const p6 = endpoint1.execute(init6);
     expect((await p5).result).toBe('hello world 4');
     expect((await p6).result).toBe('hello world 5');
     expect(fetchMock).toHaveBeenCalledTimes(6);
     // Headers defined in different order should be considered the same
-    const p7 = action1
-        .prepare()
-        .execute({ headers: new Headers({ 'x-test1': '1', 'x-test2': '2' }) });
-    const p8 = action1
-        .prepare()
-        .execute({ headers: new Headers({ 'x-test2': '2', 'x-test1': '1' }) });
+    const p7 = endpoint1.execute({ headers: new Headers({ 'x-test1': '1', 'x-test2': '2' }) });
+    const p8 = endpoint1.execute({ headers: new Headers({ 'x-test2': '2', 'x-test1': '1' }) });
     expect((await p7).result).toBe('hello world 6');
     expect((await p8).result).toBe('hello world 6');
     expect(await p7).toEqual(await p8);
@@ -75,22 +71,22 @@ test('should de-dupe in flight requests including query parameters in any order'
             },
         })
     );
-    const action1 = new Endpoint(new UrlPattern('/whatever/'), {
+    const endpoint1 = new Endpoint(new UrlPattern('/whatever/'), {
         middleware: [dedupeInFlightRequestsMiddleware()],
     });
-    const p1 = action1.execute({ query: { param1: '1', param2: '2' } });
-    const p2 = action1.execute({ query: { param2: '2', param1: '1' } });
+    const p1 = endpoint1.execute({ query: { param1: '1', param2: '2' } });
+    const p2 = endpoint1.execute({ query: { param2: '2', param1: '1' } });
     expect((await p1).result).toBe('hello world 0');
     expect((await p2).result).toBe('hello world 0');
     expect(fetchMock).toHaveBeenCalledTimes(1);
-    const p3 = action1.execute({ query: { param1: '1', param2: '2' } });
-    const p4 = action1.execute({ query: { param2: 'two', param1: '1' } });
+    const p3 = endpoint1.execute({ query: { param1: '1', param2: '2' } });
+    const p4 = endpoint1.execute({ query: { param2: 'two', param1: '1' } });
     expect((await p3).result).toBe('hello world 1');
     expect((await p4).result).toBe('hello world 2');
     expect(fetchMock).toHaveBeenCalledTimes(3);
-    const p5 = action1.execute({ query: { param1: ['one', 'two'], param2: 'test?this?' } });
-    const p6 = action1.execute({ query: { param1: ['one', 'two'], param2: 'test?this?' } });
-    const p7 = action1.execute({ query: { param1: 'one', param2: 'test' } });
+    const p5 = endpoint1.execute({ query: { param1: ['one', 'two'], param2: 'test?this?' } });
+    const p6 = endpoint1.execute({ query: { param1: ['one', 'two'], param2: 'test?this?' } });
+    const p7 = endpoint1.execute({ query: { param1: 'one', param2: 'test' } });
     expect((await p5).result).toBe('hello world 3');
     expect((await p6).result).toBe('hello world 3');
     expect((await p7).result).toBe('hello world 4');
@@ -114,7 +110,7 @@ test('should support customising key', async () => {
             },
         })
     );
-    const action1 = new Endpoint(new UrlPattern('/whatever/'), {
+    const endpoint1 = new Endpoint(new UrlPattern('/whatever/'), {
         middleware: [
             dedupeInFlightRequestsMiddleware({
                 // Set key to ignore headers, only care about url
@@ -122,17 +118,17 @@ test('should support customising key', async () => {
             }),
         ],
     });
-    const p1 = action1.prepare().execute();
-    const p2 = action1.prepare().execute();
-    const p3 = action1.prepare().execute({ headers: { 'x-test': '123' } });
+    const p1 = endpoint1.execute();
+    const p2 = endpoint1.execute();
+    const p3 = endpoint1.execute({ headers: { 'x-test': '123' } });
     expect((await p1).result).toBe('hello world 0');
     expect((await p2).result).toBe('hello world 0');
     expect((await p3).result).toBe('hello world 0');
     expect(fetchMock).toHaveBeenCalledTimes(1);
-    expect((await action1.prepare().execute()).result).toBe('hello world 1');
+    expect((await endpoint1.execute()).result).toBe('hello world 1');
     expect(fetchMock).toHaveBeenCalledTimes(2);
-    const p4 = action1.prepare().execute();
-    const p5 = action1.prepare().execute({ method: 'post' });
+    const p4 = endpoint1.execute();
+    const p5 = endpoint1.execute({ method: 'post' });
     expect((await p4).result).toBe('hello world 2');
     expect((await p5).result).toBe('hello world 3');
     expect(fetchMock).toHaveBeenCalledTimes(4);
@@ -150,7 +146,7 @@ test('should support customising test', async () => {
             },
         })
     );
-    const action1 = new Endpoint(new UrlPattern('/whatever/'), {
+    const endpoint1 = new Endpoint(new UrlPattern('/whatever/'), {
         middleware: [
             dedupeInFlightRequestsMiddleware({
                 // All requests are subject to dedupe regardless of method
@@ -159,13 +155,13 @@ test('should support customising test', async () => {
             }),
         ],
     });
-    const p1 = action1.prepare().execute();
-    const p2 = action1.prepare().execute({ method: 'POST' });
-    const p3 = action1.prepare().execute({ method: 'PUT' });
+    const p1 = endpoint1.execute();
+    const p2 = endpoint1.execute({ method: 'POST' });
+    const p3 = endpoint1.execute({ method: 'PUT' });
     expect((await p1).result).toBe('hello world 0');
     expect((await p2).result).toBe('hello world 0');
     expect((await p3).result).toBe('hello world 0');
     expect(fetchMock).toHaveBeenCalledTimes(1);
-    expect((await action1.prepare().execute()).result).toBe('hello world 1');
+    expect((await endpoint1.execute()).result).toBe('hello world 1');
     expect(fetchMock).toHaveBeenCalledTimes(2);
 });
